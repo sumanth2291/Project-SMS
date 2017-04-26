3506840a298fe62b30e9f2cd2ce92ca31b228530
Sumanth <sumanth2291@gmail.com>
2017-04-26 14:27:52 +0200

    First Commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/sumanth2291/pen/MpdNMX.
+
+ It is a messaging application for small businesses for marketing/promotions.
---
+++ js/index.js
@@ -0,0 +1,11 @@
+// For click here links
+$(document).ready(function(){
+  // For hiding Signin
+  $('#signInClick').click(function(){
+    document.querySelector('.overlay').style.display = "none";
+  });
+  // For hiding Signup
+  $('#signUpClick').click(function(){
+    document.querySelector('.overlay1').style.display = "none";
+  });
+});
